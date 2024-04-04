e72251dde71fcb1038dc12efefe66e42ddb8eda5
Triệu <lequoctrieu618@gmail.com>
2024-04-04 23:05:16 +0700

    add env to data-source

--- db/data-source.ts
+++ db/data-source.ts
@@ -2,11 +2,11 @@ import { DataSourceOptions, DataSource } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 33061,
-  username: 'root',
-  password: 'trieudz123',
-  database: 'mysql_test',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 33061,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'trieudz123',
+  database: process.env.DB_DATABASE || 'mysql_test',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
   synchronize: false,
